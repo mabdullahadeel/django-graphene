3c0bf9a3a09abb38b07d2ca9b7de627c794db980
Abdullah Adeel <64294045+mabdullahadeel@users.noreply.github.com>
2022-01-14 11:52:26 +0000

    ✅ added models for .

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Manufacturer(models.Model):
+  name = models.CharField(max_length=30, unique=True)
+  logo_url = models.CharField(max_length=256)
+
+  def __str__(self):
+    return self.name
+
+
+class Car(models.Model):
+  CAR_COLOR_OPTONS = (
+    ('white', 'white'),
+    ('red', 'red'),
+    ('green', 'green'),
+    ('grey', 'grey'),
+    ('grey', 'grey'),
+    ('blue', 'blue'),
+    ('orange', 'orange'),
+    ('orange', 'orange'),
+    ('black', 'black'),
+  )
+
+  name = models.CharField(max_length=256)
+  features_description = models.TextField()
+  manufacturer = models.ForeignKey(to=Manufacturer, on_delete=models.CASCADE, related_name="manufacturer")
+  color = models.CharField(max_length=256, choices=CAR_COLOR_OPTONS)
+
+  def __str__(self):
+    return self.name
